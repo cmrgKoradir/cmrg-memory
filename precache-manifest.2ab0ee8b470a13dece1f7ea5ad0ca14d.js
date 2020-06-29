1c03a5b61402f5a427a1fcd6cc491d234d9dcd5a
Roger Koradi <roger.koradi@comerge.net>
2020-06-29 15:21:12 +0200

    Updates

---
+++ precache-manifest.2ab0ee8b470a13dece1f7ea5ad0ca14d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8025e6e954cc64ebc2f64a7cc3c6d824",
+    "url": "/cmrg-memory/index.html"
+  },
+  {
+    "revision": "9c92296c66917cfb27b5",
+    "url": "/cmrg-memory/static/css/main.6b211dc6.chunk.css"
+  },
+  {
+    "revision": "c721e3ec323300aa33bc",
+    "url": "/cmrg-memory/static/js/2.19d7a0a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cmrg-memory/static/js/2.19d7a0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c92296c66917cfb27b5",
+    "url": "/cmrg-memory/static/js/main.33954519.chunk.js"
+  },
+  {
+    "revision": "a180d619d92a4ed34475",
+    "url": "/cmrg-memory/static/js/runtime-main.6d75802c.js"
+  }
+]);
