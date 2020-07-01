d123d4a9c0ade37bbc757f1e6f2baa09fac8534d
Roger Koradi <roger.koradi@comerge.net>
2020-07-01 12:52:41 +0200

    CWW-1721 Add meta tag for mobile scaling
    Requires `npm install --save react-helmet`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { Helmet } from 'react-helmet';
 import * as serviceWorker from './serviceWorker';
 
 import App from './App';
@@ -9,15 +10,20 @@ import About from './components/pages/About'
 import Header from './components/layout/Header';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Store>
-      <Router basename = {process.env.PUBLIC_URL}>
-        <Route path = "/" component={Header} />
-        <Route exact path="/" component= {App} />
-        <Route path="/about" component = {About} />
-      </Router>
-    </Store>
-  </React.StrictMode>,
+  <>
+    <Helmet>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Helmet>
+    <React.StrictMode>
+      <Store>
+        <Router basename = {process.env.PUBLIC_URL}>
+          <Route path = "/" component={Header}/>
+          <Route exact path="/" component= {App} />
+          <Route path="/about" component = {About} />
+        </Router>
+      </Store>
+    </React.StrictMode>
+  </>,
   document.getElementById('root')
 );
 
