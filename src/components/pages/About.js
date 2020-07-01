59aa502185acb75788a780bd6a4a5c42aeedc257
Roger Koradi <roger.koradi@comerge.net>
2020-07-01 23:35:17 +0200

    CWW-1721 Some padding for the about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 const About = () => (
     <>
-        <h1>About</h1>
-        <p>This is a simple memory game meant as an exercise to get familiar with JavaScript, React and PWAs.</p>
-        <p>It will crawl <a href="https://www.comerge.net/team">the Comerge Team Page</a> for updates when it's online
-        and use the profiles there to populate the cards with.</p>
+        <div style={containerStyle}>
+            <h1>About</h1>
+            <p>This is a simple memory game meant as an exercise to get familiar with JavaScript, React and PWAs.</p>
+            <p>It will crawl <a href="https://www.comerge.net/team">the Comerge Team Page</a> for updates when it's online
+            and use the profiles there to populate the cards with.</p>
+        </div>
     </>
 );
+
+const containerStyle = {
+    padding: '1em'
+}
 
 export default About
